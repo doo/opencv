96e9fd5a9aba68336480975af007b9c3cbadb97c
Sebastian Husche <sebastian@scanbot.io>
2020-05-27 19:41:29 +0200

    Fix build script

--- build_iOS.sh
+++ build_iOS.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-BUILD_DIR=iOS-Build/Full
+BUILD_DIR=iOS-Build/Minimal/
 BUILD_SCRIPT=./platforms/ios/build_framework.py
+CONTRIB_DIR=./contrib
 
 echo "Removing cmake build..."
 rm -dfr "${BUILD_DIR}"
@@ -10,6 +11,56 @@ echo "Setting execute flag on build script..."
 chmod +x "${BUILD_SCRIPT}"
 
 echo "Executing build script..."
-python "${BUILD_SCRIPT}" "${BUILD_DIR}" --without calib3d --without cudaarithm --without cudabsegm --without cudacodec --without cudafeatures2d --without cudafilters --without cudaimgproc --without cudalegacy --without cudaobjdetect --without cudaoptflow --without cudastereo --without cudawarping --without cudev --without dnn --without flann --without java --without js --without python --without shape --without stitching --without superres --without ts --without video --without videoio --without videostab --without viz --without world
+python "${BUILD_SCRIPT}" "${BUILD_DIR}" --debug_info --contrib "${CONTRIB_DIR}" \
+--without dnn \
+--without ml \
+--without optflow \
+--without photo \
+--without rgbd \
+--without saliency \
+--without shape \
+--without stitching \
+--without surface_matching \
+--without videoio \
+--without videostab \
+--without world \
+--without aruco \
+--without bgsegm \
+--without bioinspired \
+--without ccalib \
+--without dpm \
+--without fuzzy \
+--without hfs \
+--without img_hash \
+--without line_descriptor \
+--without phase_unwrapping \
+--without plot \
+--without reg \
+--without xfeatures2d \
+--without xobjdetect \
+--without xphoto \
+--without cudaarithm \
+--without cudabsegm \
+--without cudacodec \
+--without cudafeatures2d \
+--without cudafilters \
+--without cudaimgproc \
+--without cudalegacy \
+--without cudaobjdetect \
+--without cudaoptflow \
+--without cudastereo \
+--without cudawarping \
+--without cudev \
+--without java \
+--without js \
+--without superres \
+--without ts \
+--without video \
+--without viz \
+--without world \
+--without python \
+--disable OPENCL \
+--disable LAPACK \
+--disable EIGEN
 
 echo "Finished build script"
